37753809bab07f2e346d02cbd4f09a272a46a296
xiaolong <xiaolong@creditease.cn>
2018-01-03 15:04:06 +0800

    添加公司matcher

--- tutorials/invest/MentionExtraction.py
+++ tutorials/invest/MentionExtraction.py
@@ -1,21 +1,24 @@
-#encoding=utf-8
+# encoding=utf-8
 import os
 import numpy as np
-from snorkel import  SnorkelSession
-from snorkel.parser import CorpusParser
-
-
-
-os.environ['SNORKELHOME']='/home/lx/longxiao/workspace/knowledgegraph/snorkel/snorkel'
 from snorkel import SnorkelSession
+from snorkel.parser import CorpusParser
+from .nlp.YisouParser import LTPParser
+from snorkel.candidates import Ngrams, CandidateExtractor
+from .nlp.YisouMatcher import ORGMatcher
+
+os.environ['SNORKELHOME'] = '/home/lx/longxiao/workspace/knowledgegraph/snorkel/snorkel'
+from snorkel import SnorkelSession
+
 session = SnorkelSession()
-#load docs
+# load docs
 from .parser.Preprocessors import CSVDocPreprocessor
-#TODO 待处理的文本
-docs =CSVDocPreprocessor('/home/lx/longxiao/workspace/knowledgegraph/CNdeepdive/transaction/input/articles.csv')
+
+# TODO 待处理的文本
+docs = CSVDocPreprocessor('/home/lx/longxiao/workspace/knowledgegraph/CNdeepdive/transaction/input/articles.csv')
 
 # TODO needs specify the parser
-corpusParser = CorpusParser()
+corpusParser = CorpusParser(parser=LTPParser())
 # 文本已经作好解析
 corpusParser.apply(docs)
 
@@ -24,18 +27,14 @@ from snorkel.models import Document, Sentence
 print("Documents:", session.query(Document).count())
 print("Sentences:", session.query(Sentence).count())
 
-#下面开始提取mention
-from snorkel.models import Document,Sentence,candidate_subclass
-invest = candidate_subclass('invset',['company1','company2'])
+# 下面开始提取mention
+from snorkel.models import Document, Sentence, candidate_subclass
+
+invest = candidate_subclass('invset', ['company1', 'company2'])
 
 
 
-from snorkel.candidates import Ngrams, CandidateExtractor
-from snorkel.matchers import OrganizationMatcher
-ngrams         = Ngrams(n_max=7)
+ngrams = Ngrams(n_max=7)
 # TODO  这个地方需要改
-matcher = OrganizationMatcher(longest_match_only=True)
-candExtrator = CandidateExtractor(invest,[ngrams,ngrams],[matcher,matcher])
-
-
-
+matcher = ORGMatcher(longest_match_only=True)
+candExtrator = CandidateExtractor(invest, [ngrams, ngrams], [matcher, matcher])
---
+++ tutorials/invest/nlp/YisouMatcher.py
@@ -0,0 +1,7 @@
+from snorkel.matchers import RegexMatchEach,RegexMatch,Matcher
+
+class ORGMatcher(RegexMatchEach):
+    def __init__(self, *children, **kwargs):
+        kwargs['attrib'] = 'pos_tags'
+        kwargs['rgx'] = 'ni|nz'
+        super(ORGMatcher, self).__init__(*children, **kwargs)
