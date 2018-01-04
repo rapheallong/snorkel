f67eaea9baf2cf5c404b3dfc049aca5894eb7845
rapheallong <250811452@qq.com>
2018-01-04 13:40:02 +0800

    提取公司实体

--- tutorials/invest/LFS.py
+++ tutorials/invest/LFS.py
@@ -3,6 +3,6 @@ from snorkel.lf_helpers import (
     get_left_tokens, get_right_tokens, get_between_tokens,
     get_text_between, get_tagged_text,
 )
-
+LFS=[]
 def LF_between_words(c):
     return 1
--- tutorials/invest/TrainLFS.py
+++ tutorials/invest/TrainLFS.py
@@ -5,9 +5,13 @@ import os
 # os.environ['SNORKELDB'] = 'postgres:///snorkel-intro'
 
 import numpy as np
+from .LFS import *
+from .LFS import LFS
 from snorkel import SnorkelSession
+from snorkel.annotations import LabelAnnotator
 session = SnorkelSession()
 
 from snorkel.models import Document, Sentence, candidate_subclass
 
 invest = candidate_subclass('invset', ['company1', 'company2'])
+labeler = LabelAnnotator(lfs=LFS)
