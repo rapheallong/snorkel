02d424a52db9686d24ea0e6c4e8d56c78d505b16
rapheallong <250811452@qq.com>
2018-01-04 12:42:58 +0800

    preprocessor

---
+++ tutorials/invest/processor/Preprocessor.py
@@ -0,0 +1,14 @@
+from snorkel.parser import  DocPreprocessor
+from snorkel.models import Document
+import codecs
+
+class CSVDocPreprocessor(DocPreprocessor):
+    def parse_file(self, fp, file_name):
+        with codecs.open(fp, encoding=self.encoding) as csv:
+            for line in csv:
+                name,text = line.rsplit(',', 2)
+                stable_id = self.get_stable_id(name)
+                doc = Document(
+                    name=name, stable_id=stable_id, meta={'file_name': file_name}
+                )
+                yield doc, text
